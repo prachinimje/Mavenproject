f4c34f631732b324d6a8979514b4b432a05e8fb0
prachinimje <prachinimje1995@gmail.com>
2021-07-04 14:14:38 +0530

    adding new maven project

---
+++ src/main/java/com/jbk/Demo1.java
@@ -0,0 +1,21 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void test01(){
+		System.out.println("Hello.....");
+	}
+	@Test
+	public void test02(){
+		System.out.println("Hello World.....");
+	}
+	
+	@Test
+	public void test03(){
+		System.out.println("Hiii.....");
+	}
+
+}
